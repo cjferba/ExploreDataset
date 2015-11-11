bf19649c054b812915c173c9a79022b83069c8fd
Carlos Jesus Fernandez Basso <cjferba@gmail.com>
2015-11-11 09:24:31 +0100

    3 datasets

--- ui.R
+++ ui.R
@@ -31,22 +31,41 @@ shinyUI(navbarPage("Analisis de Dataset",
                    #################################################################################                   
                    ###                           Component 2                                     ###
                    #################################################################################                   
-                  tabPanel("Dataset",
+                  tabPanel("Dataset forest_equidepth",
                            sidebarPanel(
-                             selectInput("Feature", "feature one:", 
+                             selectInput("Feature_fe", "feature one:", 
                                          choices = c(names(forest_equidepth))),
-                             selectInput("Feature2", "Feature second:", 
+                             selectInput("Feature2__fe", "Feature second:", 
                                          choices = c(names(forest_equidepth)))
                            ),
                            mainPanel(
                              tabsetPanel(
-                               tabPanel("Histogram_forest_equidepth", plotOutput("Histogram_2")), 
+                               tabPanel("Histogram", plotOutput("Histogram_2")), 
                                tabPanel("boxplot", tableOutput("table_2")),
                                tabPanel("Summary", verbatimTextOutput("summary_2")),
                                tabPanel("Table", tableOutput("Table_2"))
                              )
                            )
                            ),
+                  #################################################################################                   
+                  ###                           Component 3                                     ###
+                  #################################################################################                   
+                  tabPanel("Dataset German",
+                           sidebarPanel(
+                             selectInput("Feature_Ge", "feature one:", 
+                                         choices = c(names(forest_equidepth))),
+                             selectInput("Feature2__Ge", "Feature second:", 
+                                         choices = c(names(forest_equidepth)))
+                           ),
+                           mainPanel(
+                             tabsetPanel(
+                               tabPanel("Histogram", plotOutput("Histogram_2")), 
+                               tabPanel("boxplot", tableOutput("table_2")),
+                               tabPanel("Summary", verbatimTextOutput("summary_2")),
+                               tabPanel("Table", tableOutput("Table_2"))
+                             )
+                           )
+                  ),
                    navbarMenu("More",
                               tabPanel("Sub-Component A"),
                               tabPanel("Sub-Component B"))
