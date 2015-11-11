264292e3452bf3abe26cd8344bf6f7f08fd7cedd
Carlos Jesus Fernandez Basso <cjferba@gmail.com>
2015-11-11 11:23:52 +0100

    new

--- server.R
+++ server.R
@@ -5,21 +5,28 @@ shinyServer(function(input, output, session) {
   
   # Combine the selected variables into a new data frame
   selectedData <- reactive({
-#     s<-unlist(strsplit(input$Feature2, split=" "))
-#     print(s)
-#     print (sum(names(bank)  == s))
-#     bank[,names(bank)  == s]
-    # bank[[input$Feature]]
-    datos <- rbind(data.frame(Feature=input$Feature, feature=bank[[input$Feature]]),
+    
+    #########################          BANK           #########################   
+    datos_bank <- rbind(data.frame(Feature=input$Feature, feature=bank[[input$Feature]]),
                 data.frame(Feature=input$Feature2, feature=bank[[input$Feature2]]))
-    datos
+    datos_bank
+    #########################    FOREST EQUIDEPTH     #########################   
+#     datos_fe <- rbind(data.frame(Feature=input$Feature_fe, feature=bank[[input$Feature_fe]]),
+#                    data.frame(Feature=input$Feature2_fe, feature=bank[[input$Feature2_fe]]))
+#     
+#     list(datos_bank,datos_bank)
   })
-  
-  
-  output$Histogram <- renderPlot({
+  ################################################################################# 
+  ###                           Component 1                                     ###
+  #################################################################################                   
+    output$Histogram <- renderPlot({
     # if(input$Graf_Bank=="Histograma"){
       # ggplot(bank, aes_string(x=input$Feature)) + geom_histogram()
-    ggplot(selectedData(), aes(x=feature, fill = Feature)) +geom_density(alpha = 0.8)
+    print("bien")
+    datos_bank <- rbind(data.frame(Feature=input$Feature, feature=bank[[input$Feature]]),
+                      data.frame(Feature=input$Feature2, feature=bank[[input$Feature2]]))
+    print("bien")
+    ggplot(datos_bank, aes(x=feature, fill = Feature)) +geom_density(alpha = 0.8)
 #     }else{
 #       ggplot(bank,aes_string(x=1, y=input$Feature)) + 
 #         geom_boxplot(aes_string(fill=1)) +
@@ -38,10 +45,10 @@ shinyServer(function(input, output, session) {
   output$summary <- renderPrint({
     tipos<-unique(selectedData()[["Feature"]])
     if (length(tipos)==1) {
-      s<-data.frame(rbind(c(name=input$Feature,summary(selectedData()[selectedData()[["Feature"]]==tipos[1],2]))))
+      s<-data.frame(rbind(c(name=input$Feature,summary(selectedData()[1][selectedData()[1][["Feature"]]==tipos[1],2]))))
     }else{
-      s<-data.frame(cbind(name=c(input$Feature,input$Feature2),rbind(summary(selectedData()[selectedData()[["Feature"]]==tipos[1],2]),
-                                                     summary(selectedData()[selectedData()[["Feature"]]==tipos[2],2]))))
+      s<-data.frame(cbind(name=c(input$Feature,input$Feature2),rbind(summary(selectedData()[1][selectedData()[1][["Feature"]]==tipos[1],2]),
+                                                     summary(selectedData()[1][selectedData()[1][["Feature"]]==tipos[2],2]))))
     }
  
     s
@@ -49,9 +56,34 @@ shinyServer(function(input, output, session) {
 #   
   # Generate an HTML table view of the data
    output$Table <- renderTable({
-     selectedData()
+     selectedData()[1]
    })
   
+   
+   ################################################################################# 
+   ###                           Component 2                                     ###
+   #################################################################################                   
+   
+   output$Histogram <- renderPlot({
+     ggplot(selectedData()[2], aes(x=feature, fill = Feature)) +geom_density(alpha = 0.8)
+   })
+   # Generate a summary of the data
+   output$summary <- renderPrint({
+     tipos<-unique(selectedData()[["Feature"]])
+     if (length(tipos)==1) {
+       s<-data.frame(rbind(c(name=input$Feature,summary(selectedData()[selectedData()[["Feature"]]==tipos[1],2]))))
+     }else{
+       s<-data.frame(cbind(name=c(input$Feature,input$Feature2),rbind(summary(selectedData()[selectedData()[["Feature"]]==tipos[1],2]),
+                                                                      summary(selectedData()[selectedData()[["Feature"]]==tipos[2],2]))))
+     }
+     
+     s
+   })
+   #   
+   # Generate an HTML table view of the data
+   output$Table <- renderTable({
+     selectedData()
+   })
 })
 
 
--- ui.R
+++ ui.R
@@ -2,9 +2,14 @@ library(shiny)
 library(ggplot2)
 
 #data<-read.csv("Data/FitnessDeepLearning.csv")
-bank<-read.csv(paste("Data/fuzzy/bank.csv"))
+if(!("bank" %in% ls())){
+  bank<-read.csv(paste("Data/fuzzy/bank.csv"))
+}
+if(!("forest_equidepth" %in% ls())){
+  forest_equidepth<-read.csv(paste("Data/fuzzy/forest-equidepth.csv"))
+}
+#if(!("bank" %in% ls())){}
 #german<-read.csv(paste("Data/fuzzy/german.csv"))
-forest_equidepth<-read.csv(paste("Data/fuzzy/forest-equidepth.csv"))
 #bank<-read.csv(paste("Data/fuzzy/bank.csv"))
 
 shinyUI(navbarPage("Analisis de Dataset",
@@ -31,41 +36,41 @@ shinyUI(navbarPage("Analisis de Dataset",
                    #################################################################################                   
                    ###                           Component 2                                     ###
                    #################################################################################                   
-                  tabPanel("Dataset forest_equidepth",
-                           sidebarPanel(
-                             selectInput("Feature_fe", "feature one:", 
-                                         choices = c(names(forest_equidepth))),
-                             selectInput("Feature2__fe", "Feature second:", 
-                                         choices = c(names(forest_equidepth)))
-                           ),
-                           mainPanel(
-                             tabsetPanel(
-                               tabPanel("Histogram", plotOutput("Histogram_2")), 
-                               tabPanel("boxplot", tableOutput("table_2")),
-                               tabPanel("Summary", verbatimTextOutput("summary_2")),
-                               tabPanel("Table", tableOutput("Table_2"))
-                             )
-                           )
-                           ),
+#                   tabPanel("Dataset forest_equidepth",
+#                            sidebarPanel(
+#                              selectInput("Feature_fe", "feature one:", 
+#                                          choices = c(names(forest_equidepth))),
+#                              selectInput("Feature2__fe", "Feature second:", 
+#                                          choices = c(names(forest_equidepth)))
+#                            ),
+#                            mainPanel(
+#                              tabsetPanel(
+#                                tabPanel("Histogram1", plotOutput("Histogram_2")), 
+#                                tabPanel("boxplot1", tableOutput("table_2")),
+#                                tabPanel("Summary1", verbatimTextOutput("summary_2")),
+#                                tabPanel("Table1", tableOutput("Table_2"))
+#                              )
+#                            )
+#                            ),
                   #################################################################################                   
                   ###                           Component 3                                     ###
                   #################################################################################                   
-                  tabPanel("Dataset German",
-                           sidebarPanel(
-                             selectInput("Feature_Ge", "feature one:", 
-                                         choices = c(names(forest_equidepth))),
-                             selectInput("Feature2__Ge", "Feature second:", 
-                                         choices = c(names(forest_equidepth)))
-                           ),
-                           mainPanel(
-                             tabsetPanel(
-                               tabPanel("Histogram", plotOutput("Histogram_2")), 
-                               tabPanel("boxplot", tableOutput("table_2")),
-                               tabPanel("Summary", verbatimTextOutput("summary_2")),
-                               tabPanel("Table", tableOutput("Table_2"))
-                             )
-                           )
-                  ),
+#                   tabPanel("Dataset German",
+#                            sidebarPanel(
+#                              selectInput("Feature_Ge", "feature one:", 
+#                                          choices = c(names(forest_equidepth))),
+#                              selectInput("Feature2__Ge", "Feature second:", 
+#                                          choices = c(names(forest_equidepth)))
+#                            ),
+#                            mainPanel(
+#                              tabsetPanel(
+#                                tabPanel("Histogram2", plotOutput("Histogram_2")), 
+#                                tabPanel("boxplot2", tableOutput("table_2")),
+#                                tabPanel("Summary2", verbatimTextOutput("summary_2")),
+#                                tabPanel("Table2", tableOutput("Table_2"))
+#                              )
+#                            )
+#                   ),
                    navbarMenu("More",
                               tabPanel("Sub-Component A"),
                               tabPanel("Sub-Component B"))
